452aa578b5f8b6f2e6632e7007dd3a6b7a731ba5
Eduardo Quagliato <eduardo@quagliato.me>
2017-09-29 21:08:03 -0300

    force an error

---
+++ README.md
@@ -0,0 +1,11 @@
+# f2c-quagliato
+
+This package returns a celsius value converted from a 
+fahrenheit value
+
+## Usage
+
+```javascript
+const f2c = require('f2c-quagliato')
+const tempC = f2c(tempF)
+```
---
+++ index.js
@@ -0,0 +1,11 @@
+// index.js
+module.exports = function (tempF) {
+  if (typeof tempF === 'string') {
+  	tempF = tempF.trim().replace(',', '.')
+  }
+  let tempC = parseFloat(tempF)
+  tempC -= 32
+  tempC /= 1.8
+
+  return tempC.toFixed(2)
+}
